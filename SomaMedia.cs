ee75705d745aa0e2fbe66079c50ddad816849e4b
isabelsantana <50423863+isabelsantana@users.noreply.github.com>
2019-11-04 11:54:27 -0300

    Add files via upload

---
+++ SomaMedia.cs
@@ -0,0 +1,21 @@
+﻿//Faça um programa que leia 5 números e informe a soma e a média dos números.
+
+using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int numero, media, soma = 0;
+
+        for (int i = 0; i < 5; i = i + 1)
+        {
+            Console.WriteLine("Digite um número: ");
+            numero = int.Parse(Console.ReadLine());
+            soma = soma + numero;
+
+        }
+        media = soma / 5;
+        Console.WriteLine("A média é:{0}", media);
+    }
+}

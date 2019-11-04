04605837f9667b40b528c83743f4800793711654
isabelsantana <50423863+isabelsantana@users.noreply.github.com>
2019-11-04 11:58:25 -0300

    Add files via upload

---
+++ VetorPrimos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class MainClass{
+    public static bool Primo(int numero){
+
+        int qtdDivisores = 0;
+
+        for (int i = numero; i > 0; i--){
+            if (numero % i == 0)
+                qtdDivisores++;
+        }
+
+        return qtdDivisores > 2 ? false : true;
+    }
+
+    static void Main(string[] args){
+
+        int[] vetor = new int[] { 1, 8, 15, 7, 17, 1, 4, 2, 3, 12 };
+
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            if (Primo(vetor[i]))
+                Console.WriteLine("Elemento do índice {0}: {1}", i, vetor[i]);
+        }
+
+        Console.ReadKey();
+    }
+
+}

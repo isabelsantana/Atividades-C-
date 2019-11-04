f089278cf587d553d3a11dcc1dd23cd3cecdccbf
isabelsantana <50423863+isabelsantana@users.noreply.github.com>
2019-11-04 11:52:52 -0300

    Add files via upload

---
+++ NumeroImpar.cs
@@ -0,0 +1,15 @@
+﻿//Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
+
+using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        for (int impar = 1; impar <= 50; impar = impar + 2)
+        {
+            Console.WriteLine(impar);
+        }
+    }
+}

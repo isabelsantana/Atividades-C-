2a7f65ac0053ea5884bdeaa5aacaf36f75194099
isabelsantana <50423863+isabelsantana@users.noreply.github.com>
2019-11-04 11:48:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Faça um programa que leia 5 números e informe o maior número.
+
+
+using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int numero, maior;
+
+        numero = int.Parse(Console.ReadLine());
+        maior = numero;
+
+        for (int i = 0; i < 5; i = i + 1)
+        {
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero > maior)
+            {
+                maior = numero;
+            }
+        }
+        Console.WriteLine(maior);
+
+    }
+}
+

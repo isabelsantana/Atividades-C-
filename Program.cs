9fb886a9858e1204928fff986b5b820c32fb90dc
isabelsantana <50423863+isabelsantana@users.noreply.github.com>
2019-11-08 23:24:39 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        double a = 1.0;
+        double b = 3.0;
+        Console.WriteLine(a / b);
+
+        decimal c = 1.0M;
+        decimal d = 3.0M;
+        Console.WriteLine(c / d);
+    }
+}

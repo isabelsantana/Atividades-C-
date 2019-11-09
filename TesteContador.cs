906877efab15642d60c570666c6f643a7f8a354a
isabelsantana <50423863+isabelsantana@users.noreply.github.com>
2019-11-08 23:29:57 -0300

    Add files via upload

---
+++ TesteContador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int contador = 0;
+        while (contador <= 100)
+        {
+
+            Console.WriteLine($"O contador é {contador}");
+            contador++;
+        }
+    }
+}

46e887f8ed90585305ab75d05c7d20ed8c1bfad2
isabelsantana <50423863+isabelsantana@users.noreply.github.com>
2019-11-08 23:21:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
+
+using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int tam = 5;
+        int[] meuVet = new int[tam];
+        for (int i = 0; i < tam; i++)
+        {
+            Console.WriteLine("Valor: {0}", i + 1);
+            meuVet[i] = int.Parse(Console.ReadLine());
+
+        }
+
+        for (int i = 0; i < tam; i++)
+        {
+            Console.Write("{0} - ", meuVet[i]);
+        }
+    }
+}

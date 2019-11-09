676774ecda1e91768253bb92a421a97a3f254e8a
isabelsantana <50423863+isabelsantana@users.noreply.github.com>
2019-11-08 23:27:47 -0300

    Add files via upload

---
+++ TesteMaiorMenor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int a = 5;
+        int b = 3;
+        int c = 4;
+
+        if ((a + b + c > 10) || (a == b))
+        {
+
+            Console.WriteLine("A resposta é maior que 10");
+            Console.WriteLine("Ou o primeiro numero é igual ao segundo");
+        }
+        else
+        {
+            Console.WriteLine("A resposta não é maior que 10");
+            Console.WriteLine("E o primeiro numero é igual ao segundo");
+        }
+    }
+}

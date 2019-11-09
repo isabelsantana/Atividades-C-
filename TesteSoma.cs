109e79d8367433d9447921a82c12f133e31792a3
isabelsantana <50423863+isabelsantana@users.noreply.github.com>
2019-11-08 23:31:17 -0300

    Add files via upload

---
+++ TesteSoma.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        int soma = 0;
+        for (int num = 1; num < 21; num++)
+        {
+
+            if (num % 3 == 0)
+            {
+
+                soma = soma + num;
+            }
+        }
+
+        Console.WriteLine($"A soma é {soma}");
+
+    }
+}

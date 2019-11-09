b67c819b6134ecab6cd2471631d7e57f1fe12852
isabelsantana <50423863+isabelsantana@users.noreply.github.com>
2019-11-08 23:32:26 -0300

    Add files via upload

---
+++ NumeroMaior.cs
@@ -0,0 +1,33 @@
+﻿//Faça um programa em C# que recebe 3 números e imprime na tela o maior dos 3 números.
+
+using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int num1, num2, num3;
+        Console.WriteLine("Digite um numero: ");
+        num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite um numero: ");
+        num2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite um numero: ");
+        num3 = int.Parse(Console.ReadLine());
+
+        if (num1 > num2 & num1 > num3)
+        {
+            Console.WriteLine("Numero 1 é o maior numero!" + num1);
+        }
+        if (num2 > num1 & num2 > num3)
+        {
+            Console.WriteLine("Número 2 é o maior numero!" + num2);
+        }
+        if (num3 > num1 & num3 > num2)
+        {
+            Console.WriteLine("Numero 3 é o maior numero!" + num3);
+        }
+
+    }
+}

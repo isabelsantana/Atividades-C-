905b12aa0355a6a5081f775fbca1fa6d05a74968
isabelsantana <50423863+isabelsantana@users.noreply.github.com>
2019-12-10 00:38:05 -0300

    Create Bola2.cs

---
+++ Bola2.cs
@@ -0,0 +1,23 @@
+//Classe Bola: Crie uma classe que modele uma bola:
+
+//Atributos: Cor, circunferência, material
+//Métodos: trocaCor e mostraCor
+
+class Bola {
+  //Atributos ou caracteristicas:
+  
+  private string cor;
+  private float circunf;
+  private string material;
+
+    public string mostraCor(){
+      return cor;
+    }
+
+    public void trocaCor(string c){
+      cor = c;
+    }
+  
+}
+
+  

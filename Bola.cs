24490829fb71f8f94198bf404c4ae200f083a745
isabelsantana <50423863+isabelsantana@users.noreply.github.com>
2019-12-10 00:37:45 -0300

    Create Bola.cs

---
+++ Bola.cs
@@ -0,0 +1,11 @@
+using System;
+  class MainClass {
+    
+    public static void Main (string[] args) {
+    
+   Bola cor = new Bola(); 
+    cor.trocaCor("azul");
+   
+    Console.WriteLine(cor.mostraCor());
+   }
+ }
